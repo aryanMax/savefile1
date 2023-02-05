d7e7e72251959ef5a92f27567bdb9c6659c302b8
Aryan Rana <105802182+aryanMax@users.noreply.github.com>
2023-02-05 17:11:43 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/aryanMax/savefile1 /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 -m main
